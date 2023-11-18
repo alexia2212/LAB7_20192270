a2ecc89c149e528069ba1007de569ac973b18944
alexia2212 <a20192270@pucp.edu.pe>
2023-11-18 12:49:30 -0500

    aaaaaaaa

--- app/src/main/java/com/example/lab7_20192270/MainActivity.java
+++ app/src/main/java/com/example/lab7_20192270/MainActivity.java
@@ -1,23 +1,86 @@
 package com.example.lab7_20192270;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.lab7_20192270.databinding.ActivityMainBinding;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        mAuth = FirebaseAuth.getInstance();
+
         binding.button.setOnClickListener(view -> {
             Intent intent = new Intent(MainActivity.this, MenuSalon.class);
             startActivity(intent);
         });
+        /*
+        Intent intent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(Arrays.asList(
+                        new AuthUI.IdpConfig.EmailBuilder().build(),
+                        new AuthUI.IdpConfig.GoogleBuilder().build()
+                ))
+                .setIsSmartLockEnabled(false)
+                .build();
+
+        signInLauncher.launch(intent);
+*/
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null){
+            Log.d("msg","uid: " + auth.getCurrentUser().getUid());
+            Log.d("msg","name: " + auth.getCurrentUser().getDisplayName());
+            Log.d("msg","email: " + auth.getCurrentUser().getEmail());
+
+            auth.getCurrentUser().reload().addOnCompleteListener(task -> {
+                if(auth.getCurrentUser().isEmailVerified()){
+                    Log.d("msg","correo autenticado");
+                } else{
+                    auth.getCurrentUser().sendEmailVerification()
+                            .addOnCompleteListener(task2 -> Log.d("msg", "correo enviado"));
+                }
+            });
+
+        }else{
+            Intent signedIntent = AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(Arrays.asList(
+                            new AuthUI.IdpConfig.EmailBuilder().build(),
+                            new AuthUI.IdpConfig.GoogleBuilder().build()
+                    ))
+                    .setIsSmartLockEnabled(false)
+                    .build();
+        }
+
     }
+    ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            result -> {
+                if (result.getResultCode() == RESULT_OK) {
+                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                    Log.d("msg-test", "Firebase uid: " + user.getUid());
+                } else {
+                    Log.d("msg-test", "Canceló el Log-in");
+                }
+            }
+    );
+
+
 }
--- app/src/main/java/com/example/lab7_20192270/MenuSalon.java
+++ app/src/main/java/com/example/lab7_20192270/MenuSalon.java
@@ -1,11 +1,22 @@
 package com.example.lab7_20192270;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.lab7_20192270.databinding.ActivityMenuSalonBinding;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.google.firebase.auth.AuthKt;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.checkerframework.checker.interning.qual.Interned;
+
+import java.util.Arrays;
 
 public class MenuSalon extends AppCompatActivity {
 
@@ -17,8 +28,11 @@ public class MenuSalon extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         binding.buttonDatos.setOnClickListener(view -> {
-            Intent intent = new Intent(MenuSalon.this, DatosActivity.class);
-            startActivity(intent);
+            Intent intent2 = new Intent(MenuSalon.this, DatosActivity.class);
+            startActivity(intent2);
         });
+
+
     }
+
 }
